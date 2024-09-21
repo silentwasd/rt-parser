57d9c653ad042fa9ce6ff267591fea4b3d6f6626
ilya.ignatev <ilya.ignatev@onlinesup.com>
2024-09-21 18:44:25 +0300

    Search sort

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\Rt\Enums\SearchDirection;
+use App\Services\Rt\Enums\SearchOrder;
 use App\Services\Rt\RtService;
 use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class SearchController extends Controller
 {
@@ -14,11 +17,37 @@ class SearchController extends Controller
     public function __invoke(Request $request, RtService $rt)
     {
         $data = $request->validate([
-            'query' => 'required|string|max:255'
+            'query'          => 'required|string|max:255',
+            'sort_column'    => [
+                'required',
+                Rule::in(['name', 'category', 'size', 'seeds', 'leeches', 'downloads', 'created_at'])
+            ],
+            'sort_direction' => [
+                'required',
+                Rule::in(['asc', 'desc'])
+            ]
         ]);
 
+        $order = match ($data['sort_column']) {
+            'name'       => SearchOrder::TopicName,
+            'size'       => SearchOrder::Size,
+            'seeds'      => SearchOrder::Seeds,
+            'leeches'    => SearchOrder::Leeches,
+            'downloads'  => SearchOrder::Downloads,
+            'created_at' => SearchOrder::Registered
+        };
+
+        $direction = match ($data['sort_direction']) {
+            'asc'  => SearchDirection::Ascending,
+            'desc' => SearchDirection::Descending
+        };
+
         return response()->json(
-            $rt->search($data['query'])
+            $rt->search(
+                query: $data['query'],
+                order: $order,
+                direction: $direction
+            )
         );
     }
 }
