8dac09d3fd5f802d92d4f08c817b7aacc3b95b25
ilya.ignatev <ilya.ignatev@onlinesup.com>
2024-09-26 15:24:04 +0300

    Yet another forum

--- app/Jobs/ParseMovieJob.php
+++ app/Jobs/ParseMovieJob.php
@@ -53,6 +53,9 @@ class ParseMovieJob implements ShouldQueue
         return $matches;
     }
 
+    /**
+     * @throws Exception
+     */
     public function handle(RtService $rt): void
     {
         $topicModel = Topic::updateOrCreate(
@@ -67,42 +70,53 @@ class ParseMovieJob implements ShouldQueue
             ]
         );
 
-        preg_match('/^([^\[(]+)/', $topicModel->name, $titleMatch);
+        if (!preg_match('/^([^\[(]+)/', $topicModel->name, $titleMatch))
+            throw new Exception("Can't parse title");
 
         $parts = explode(" / ", $titleMatch[1]);
 
         $year = $this->extract("/\[(?<from>\d{4})(-(?<to>\d{4}))?/", $topicModel->name);
 
-        $genre = $this->extractAll("/\W(романтическая комедия|музыкальная комедия|музыкальная мелодрама|мелодрама|боевик|немое кино|комедия|драма|история|ужасы|триллер|детектив|приключения|экранизация|вестерн|исторический|военный|фантастика|антиутопия|криминал|сказка|мюзикл|фэнтези|семейный|comedy|drama|притча|биография|мистика|афёра|историко-приключенческий|детский музыкальный фильм|action|biography|history|war|рок-опера|фильм-нуар|военно-приключенческий|сатира|трагикомедия|музыкальный фильм|комедии|романс|эротика|шпионский триллер|шпионский|катастрофа|фильм нуар|нуар|трагедия|детективная пародия)+\W/iu", $topicModel->name);
+        $genre = $this->extractAll("/\W(драма\\\\документальный|боевик\/приключения|романтическая комедия|музыкальная комедия|музыкальная мелодрама|шпионский боевик|военная драма|мелодрама|боевик|немое кино|комедия|драма|история|ужасы|триллер|детектив|приключения|экранизация|вестерн|исторический|военный|фантастика|антиутопия|криминал|сказка|мюзикл|фэнтези|семейный|comedy|drama|притча|биография|мистика|афёра|историко-приключенческий|детский музыкальный фильм|action|biography|history|war|рок-опера|фильм-нуар|военно-приключенческий|сатира|трагикомедия|музыкальный фильм|комедии|романс|эротика|шпионский триллер|шпионский|катастрофа|фильм нуар|нуар|трагедия|детективная пародия|приключенческий|спорт|роуд-муви|музыка|ужас|готический хоррор|еврокрайм|трэш|эротическое мракобесие|историко-биографический фильм|детский|остросюжетный|giallo|комедийные новеллы|боевые искусства|музыкальный|постапокалипсис|приключение|fantasy|романтический)+\W/iu", $topicModel->name);
 
         $genres = collect($genre[1] ?? [])
             ->map(fn($genre) => Str::lower($genre))
             ->map(fn($genre) => match ($genre) {
-                'comedy', 'комедии'         => 'комедия',
-                'drama'                     => 'драма',
-                'action'                    => 'боевик',
-                'biography'                 => 'биография',
-                'history', 'исторический'   => 'история',
-                'war'                       => 'военный',
-                'фильм-нуар', 'фильм нуар'  => 'нуар',
-                'романтическая комедия'     => ['романтика', 'комедия'],
-                'музыкальная комедия'       => ['мюзикл', 'комедия'],
-                'музыкальная мелодрама'     => ['мюзикл', 'мелодрама'],
-                'историко-приключенческий'  => ['история', 'приключения'],
-                'детективная пародия'       => ['детектив', 'пародия'],
-                'музыкальный фильм'         => 'мюзикл',
-                'шпионский триллер'         => ['шпионский', 'триллер'],
-                'детский музыкальный фильм' => ['детский', 'мюзикл'],
-                'романс'                    => 'романтика',
-                'военно-приключенческий'    => ['военный', 'приключения'],
-                default                     => $genre
+                'comedy', 'комедии'                => 'комедия',
+                'drama'                            => 'драма',
+                'action'                           => 'боевик',
+                'biography'                        => 'биография',
+                'history', 'исторический'          => 'история',
+                'war'                              => 'военный',
+                'фильм-нуар', 'фильм нуар'         => 'нуар',
+                'романтическая комедия'            => ['романтика', 'комедия'],
+                'музыкальная комедия'              => ['мюзикл', 'комедия'],
+                'музыкальная мелодрама'            => ['мюзикл', 'мелодрама'],
+                'историко-приключенческий'         => ['история', 'приключения'],
+                'детективная пародия'              => ['детектив', 'пародия'],
+                'музыкальный фильм', 'музыкальный' => 'мюзикл',
+                'шпионский триллер'                => ['шпионский', 'триллер'],
+                'детский музыкальный фильм'        => ['детский', 'мюзикл'],
+                'романс', 'романтический'          => 'романтика',
+                'военно-приключенческий'           => ['военный', 'приключения'],
+                'драма\документальный'             => ['драма', 'документальный'],
+                'боевик/приключения'               => ['боевик', 'приключения'],
+                'историко-биографический фильм'    => ['история', 'биография'],
+                'giallo'                           => ['детектив', 'триллер'],
+                'комедийные новеллы'               => ['комедия'],
+                'шпионский боевик'                 => ['шпионский', 'боевик'],
+                'военная драма'                    => ['военный', 'драма'],
+                'приключение', 'приключенческий'   => 'приключения',
+                'fantasy'                          => 'фэнтези',
+                'ужас'                             => 'ужасы',
+                default                            => $genre
             })
             ->flatten()
             ->unique()
             ->map(fn($genre) => Genre::firstOrCreate(['name' => $genre, 'genre_type' => GenreType::Movie]))
             ->map(fn(Genre $genre) => $genre->id);
 
-        $country = $this->extractAll("/(западная германия|австрия|фрг|сша|франция|италия|германия|великобритания|бразилия|канада|испания|югославия|польша|мексика|чехословакия|ссср|израиль|швейцария|вел.бр|австралия|швеция|гдр|япония|нидерланды|дания|венгрия|болгария|финляндия|аргентина|алжир|греция|чехия|египет|румыния)+/iu", $topicModel->name);
+        $country = $this->extractAll("/(западная германия|австрия|фрг|сша|франция|италия|германия|великобритания|бразилия|канада|испания|югославия|польша|мексика|чехословакия|ссср|израиль|швейцария|вел.бр|австралия|швеция|гдр|япония|нидерланды|дания|венгрия|болгария|финляндия|аргентина|алжир|греция|чехия|египет|румыния|норвегия|ботсвана|юар|новая зеландия|исламская республика иран|турция|пакистан|иран|гонконг|португалия|албания)+/iu", $topicModel->name);
 
         $countries = collect($country[1] ?? [])
             ->map(fn($country) => Str::lower($country))
@@ -111,6 +125,9 @@ class ParseMovieJob implements ShouldQueue
                 'вел.бр'                          => 'великобритания',
                 'сша'                             => 'США',
                 'ссср'                            => 'СССР',
+                'юар'                             => 'ЮАР',
+                'исламская республика иран'       => 'иран',
+                'новая зеландия'                  => 'Новая Зеландия',
                 default                           => $country
             })
             ->flatten()
@@ -118,32 +135,42 @@ class ParseMovieJob implements ShouldQueue
             ->map(fn($country) => Country::firstOrCreate(['name' => Str::ucfirst($country)]))
             ->map(fn(Country $country) => $country->id);
 
-        $release = $this->extract("/(dvd-avc|dvd5|dvdrip-avc|dvdrip|bdrip-avc|bdrip avc|vhsrip|hdrip-avc|hdrip|bdrip|web-dlrip-avc|web-dlvrip-avc|web-dlrip|tvrip-avc|tvrip|webrip|screener|satrip|dvb|dvdremux|ldrip|telesync|telecine|betacamrip)+/iu", $topicModel->name);
+        $release = $this->extract("/(dvd-avc|dvd5|dvd9|dvdrip-avc|dvdrip|bdrip-avc|bdrip avc|vhsrip-avc|vhsrip|hdrip-avc|hdrip|bdrip|web-dlrip-avc|web-dlvrip-avc|web-dlrip|web-dl-avc|hdtvrip|tvrip-avc|tvrip|webrip|screener|satrip|dvb|dvdremux|ldrip|telesync|telecine|betacamrip|dtheaterrip|vcdrip|d-theaterrip|camrip|VHS -> DVD|ldremux|web-dl)+/iu", $topicModel->name);
 
         $releaseName = match (Str::lower($release[1] ?? '')) {
-            'dvd-avc'                => 'DVD-AVC',
-            'dvd5'                   => 'DVD5',
-            'dvdrip-avc'             => 'DVDRip-AVC',
-            'dvdrip'                 => 'DVDRip',
-            'bdrip avc', 'bdrip-avc' => 'BDRip-AVC',
-            'vhsrip'                 => 'VHSRip',
-            'hdrip-avc'              => 'HDRip-AVC',
-            'hdrip'                  => 'HDRip',
-            'bdrip'                  => 'BDRip',
-            'web-dlrip-avc'          => 'WEB-DLRip-AVC',
-            'web-dlvrip-avc'         => 'WEB-DLVRip-AVC',
-            'web-dlrip'              => 'WEB-DLRip',
-            'tvrip-avc'              => 'TVRip-AVC',
-            'tvrip'                  => 'TVRip',
-            'webrip'                 => 'WEBRip',
-            'satrip'                 => 'SATRip',
-            'dvb'                    => 'DVB',
-            'dvdremux'               => 'DVDRemux',
-            'ldrip'                  => 'LDRip',
-            'betacamrip'             => 'BetacamRip',
-            'telecine'               => 'Telecine',
-            'telesync'               => 'Telesync',
-            default                  => Str::ucfirst($release[1] ?? '')
+            'dvd-avc'                     => 'DVD-AVC',
+            'dvd9'                        => 'DVD9',
+            'dvd5'                        => 'DVD5',
+            'dvdrip-avc'                  => 'DVDRip-AVC',
+            'dvdrip'                      => 'DVDRip',
+            'bdrip avc', 'bdrip-avc'      => 'BDRip-AVC',
+            'vhsrip-avc'                  => 'VHSRip-AVC',
+            'vhsrip'                      => 'VHSRip',
+            'hdrip-avc'                   => 'HDRip-AVC',
+            'hdrip'                       => 'HDRip',
+            'bdrip'                       => 'BDRip',
+            'web-dlrip-avc'               => 'WEB-DLRip-AVC',
+            'web-dlvrip-avc'              => 'WEB-DLVRip-AVC',
+            'web-dlrip'                   => 'WEB-DLRip',
+            'web-dl-avc'                  => 'WEB-DL-AVC',
+            'web-dl'                      => 'WEB-DL',
+            'hdtvrip'                     => 'HDTVRip',
+            'tvrip-avc'                   => 'TVRip-AVC',
+            'tvrip'                       => 'TVRip',
+            'webrip'                      => 'WEBRip',
+            'satrip'                      => 'SATRip',
+            'dvb'                         => 'DVB',
+            'dvdremux'                    => 'DVDRemux',
+            'ldrip'                       => 'LDRip',
+            'betacamrip'                  => 'BetacamRip',
+            'telecine'                    => 'Telecine',
+            'telesync'                    => 'Telesync',
+            'dtheaterrip', 'd-theaterrip' => 'D-TheaterRip',
+            'vcdrip'                      => 'VCDRip',
+            'camrip'                      => 'CAMRip',
+            'ldremux'                     => 'LDRemux',
+            'VHS -> DVD'                  => 'VHS -> DVD',
+            default                       => Str::ucfirst($release[1] ?? '')
         };
 
         $releaseModel = $releaseName
